ff4e1f837bd28e8750bc1e7cd12770a8e88ab5a5
Jarek Páral <paral.jarek@gmail.com>
2018-02-06 22:15:44 +0100

    DE1_SoC_SDRAM_Nios_Test: add working example with C++ project (use std::cout, std::vector) and 64 MB SDRAM as main RAM

---
+++ DE1_SoC_SDRAM_Nios_Test/software/DE1_SoC_SDRAM_Nios_Test/main.cpp
@@ -0,0 +1,21 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 6 Feb 2018
+ *      Author: JarekParal
+ */
+
+#include "terasic_includes.h"
+#include "system.h"
+#include "alt_types.h"
+#include <iostream>
+//#include <vector>
+int main()
+{
+	printf("===== Hello world) =====\r\n");
+	//std::vector<int> vec;
+	//vec.push_back(1);
+	std::cout << "hello" << std::endl;
+
+	return 0;
+}
