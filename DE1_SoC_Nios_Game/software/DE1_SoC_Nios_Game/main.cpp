2b805fc0444997052b62e35c6adadbce315a15f9
Jarek Páral <paral.jarek@gmail.com>
2018-02-07 00:23:52 +0100

    DE1_SoC_Nios_Game: change software project to C++ - work std::cout and std::vector

---
+++ DE1_SoC_Nios_Game/software/DE1_SoC_Nios_Game/main.cpp
@@ -0,0 +1,43 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 6 Feb 2018
+ *      Author: JarekParal
+ */
+
+#include <stdio.h>
+#include <stdlib.h> // malloc, free
+#include <string.h>
+#include <stddef.h>
+#include <unistd.h>  // usleep (unix standard?)
+#include "sys/alt_flash.h"
+#include "sys/alt_flash_types.h"
+#include "io.h"
+#include "alt_types.h"  // alt_u32
+//#include "altera_avalon_pio_regs.h" //IOWR_ALTERA_AVALON_PIO_DATA
+#include "sys/alt_irq.h"  // interrupt
+#include "sys/alt_alarm.h" // time tick function (alt_nticks(), alt_ticks_per_second())
+#include "sys/alt_timestamp.h"
+#include "sys/alt_stdio.h"
+#include "system.h"
+#include <fcntl.h>
+//#include "debug.h"
+
+#define DEBUG_DUMP  /*printf */
+
+#include <iostream>
+#include <vector>
+
+int main()
+{
+	printf("===== Hello world =====\r\n");
+	std::vector<int> vec;
+	vec.push_back(1);
+	std::cout << "hello " << vec[0]  << std::endl;
+
+
+	return 0;
+}
+
+
+
